d7c761930459249fc3f19d4a46beb74d5c6cc613
yuxuan <yuxuan.zhuang@dbb.su.se>
2020-06-09 13:37:50 +0200

    add pickle function to chainreader

--- package/MDAnalysis/coordinates/chain.py
+++ package/MDAnalysis/coordinates/chain.py
@@ -378,6 +378,17 @@ class ChainReader(base.ProtoReader):
                 util.iterable(thing) and
                 not util.isstream(thing))
 
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state.pop('_ChainReader__chained_trajectories_iter', None)
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.__chained_trajectories_iter = self._chained_iterator()
+
+
     def _get_local_frame(self, k):
         """Find trajectory index and trajectory frame for chained frame `k`.
 
